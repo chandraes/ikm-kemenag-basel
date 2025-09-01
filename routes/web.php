edc19f4ceeaf9ea3b273805d3fac3bd73ee340e2
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-09-01 15:00:22 +0700

    chore: Remove unused GitHub workflows and commented routes for appearance settings

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
+// use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
@@ -23,10 +23,9 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    // Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
     Route::get('/pengaturan', PengaturanAplikasi::class)->name('pengaturan');
-
 
     Route::prefix('master')->name('master.')->group(function () {
         Route::get('satker', \App\Livewire\Satker\ManajemenSatker::class)->name('satker');
